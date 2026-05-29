ea23eb271d12012beaa588febbfa549ca6a59315
rafaeltaffo <rafael.taffo07@gmail.com>
2026-05-29 18:47:15 -0300

    feat

---
+++ main_menu.py
@@ -0,0 +1,10 @@
+def main():
+    while True:
+        print("\n1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Clear")
+        print("6. Exit")
+
+        input("\nChoose an option: ")
